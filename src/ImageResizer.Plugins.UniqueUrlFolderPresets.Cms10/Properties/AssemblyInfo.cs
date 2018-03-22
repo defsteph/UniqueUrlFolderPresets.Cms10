a6c85710d5ba610a665e8445b125c80399cbdb58
Stephan Lonntorp <stephan.lonntorp@valtech.se>
2018-03-22 21:07:42 +0100

    Fixes bug with parsing the preset

--- src/ImageResizer.Plugins.UniqueUrlFolderPresets.Cms10/Properties/AssemblyInfo.cs
+++ src/ImageResizer.Plugins.UniqueUrlFolderPresets.Cms10/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using EPiServer.PlugIn;
 [assembly: AssemblyProduct("ImageResizer.Plugins.UniqueUrlFolderPresets.Cms10")]
 [assembly: ComVisible(false)]
 [assembly: Guid("deae71af-3b0b-435d-92de-0756da29ea51")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("ImageResizer.Plugins.UniqueUrlFolderPresets.Cms10.Tests")]
 [assembly: PlugInSummary(MoreInfoUrl = "<A href='https://github.com/defsteph/UniqueUrlFolderPresets.Cms10'>GitHub Repository</A>", License = LicensingMode.Freeware)]
 #if DEBUG
